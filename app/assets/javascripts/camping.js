2581f4df605d792eb3207a13c08367bab8dc4963
FerJaraG <mfjaragalleguillos@gmail.com>
2018-03-14 17:47:21 -0300

    carrierwave

--- app/assets/javascripts/camping.js
+++ app/assets/javascripts/camping.js
@@ -1,16 +1,22 @@
+
+/*
 jQuery(function() {
   var cities;
-  cities = $('#camping_city_id').html();
+  cities = $('#city_id').html();
   console.log(regions);
-  return $('#camping_region_id').change(function() {
+  return $('#region_id').change(function() {
     var region, options;
-    region = $('#camping_region_id :selected').text();
+    region = $('#region_id :selected').text();
     options = $(cities).filter("optgroup[label=" + region + "]").html();
     console.log(options);
     if (options) {
-      return $('#camping_city_id').html(options);
+      return $('#city_id').html(options);
     } else {
-      return $('#camping_city_id').empty();
+      return $('#city_id').empty();
     }
   });
 });
+
+*/
+
+
